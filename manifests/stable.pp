64a704996b365c6f2eea1782061c0b06325912bd
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-04-23 17:49:09 -0700

    STyle

--- manifests/stable.pp
+++ manifests/stable.pp
@@ -1,6 +1,6 @@
 class droplr {
   package { 'Droplr':
     source   => 'http://files.droplr.com.s3.amazonaws.com/apps/mac/Droplr+3.0.5.zip',
-    provider => 'compressed_app'
+    provider => 'compressed_app',
   }
 }
